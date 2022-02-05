525b8b9fb0bd2dddae77323ce6c92d2ef0777e15
jf-eng <engjianfu@gmail.com>
2022-02-05 20:44:09 +0000

    integrated html

--- pages/views.py
+++ pages/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def homepage_view(request):
-    return render(request, 'home.html') #takes in template name and context
+    return render(request, 'HTMLFrontPage.html') #takes in template name and context
