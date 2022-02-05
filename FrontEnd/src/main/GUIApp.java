2bcdfb7ac8bd24f07e9a5e6bd112faefaad5804d
tahmid148 <tahmidrahman2812@outlook.com>
2022-02-05 16:03:07 +0000

    Created GUIApp class which will be used as the front-end for the program

--- FrontEnd/src/main/GUIApp.java
+++ FrontEnd/src/main/GUIApp.java
@@ -1,5 +1,32 @@
 package main;
 
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
+import org.opencv.core.Core;
+import org.opencv.videoio.VideoCapture;
+
 public class GUIApp {
+  private JFrame frame;
+  private JPanel panel;
+
+  public GUIApp() {
+    frame = new JFrame("Video Capture");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+
+
+
+  }
+
+  private void display() {
+    frame.setVisible(true);
+  }
+
+  public static void main(String[] args) {
+    GUIApp guiApp = new GUIApp();
+    guiApp.display();
+  }
 
 }
